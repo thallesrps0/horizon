ac66a3654b93e7d4e6f3f3c4faac821156319ba2
Thalles Ramon <thallesrps0@gmail.com>
2022-05-09 20:20:09 -0300

    Add files via upload

---
+++ js/scroll.js
@@ -0,0 +1,69 @@
+
+  /**
+  * Navbar links active state on scroll
+  */
+ let navbarlinks = select('#navbar .scrollto', true)
+ const navbarlinksActive = () => {
+   let position = window.scrollY + 200
+   navbarlinks.forEach(navbarlink => {
+     if (!navbarlink.hash) return
+     let section = select(navbarlink.hash)
+     if (!section) return
+     if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+       navbarlink.classList.add('active')
+     } else {
+       navbarlink.classList.remove('active')
+     }
+   })
+ }
+ window.addEventListener('load', navbarlinksActive)
+ onscroll(document, navbarlinksActive)
+
+ /**
+  * Scrolls to an element with header offset
+  */
+ const scrollto = (el) => {
+   let header = select('#header')
+   let offset = header.offsetHeight
+
+   if (!header.classList.contains('header-scrolled')) {
+     offset -= 10
+   }
+
+   let elementPos = select(el).offsetTop
+   window.scrollTo({
+     top: elementPos - offset,
+     behavior: 'smooth'
+   })
+ }
+
+ /**
+  * Toggle .header-scrolled class to #header when page is scrolled
+  */
+ let selectHeader = select('#header')
+ if (selectHeader) {
+   const headerScrolled = () => {
+     if (window.scrollY > 100) {
+       selectHeader.classList.add('header-scrolled')
+     } else {
+       selectHeader.classList.remove('header-scrolled')
+     }
+   }
+   window.addEventListener('load', headerScrolled)
+   onscroll(document, headerScrolled)
+ }
+
+ /**
+  * Animation on scroll
+  */
+ function aos_init() {
+   AOS.init({
+     duration: 1000,
+     easing: "ease-in-out",
+     once: true,
+     mirror: false
+   });
+ }
+ window.addEventListener('load', () => {
+   aos_init();
+ });
